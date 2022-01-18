2f868d4b13aa4c3e7e989d08deabd54ab029ffd1
jamiguel23 <josematthewmiguel@gmail.com>
2022-01-18 15:22:27 -0800

    changed root index file paths from requires to lib, added starter code to server.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
 // Start up DB Server
-const { db } = require('./src/auth/models/index.js');
+const { db } = require('./lib/auth/models/index.js');
 db.sync()
   .then(() => {
 
     // Start the web server
-    require('./src/server.js').start(process.env.PORT);
+    require('./lib/server.js').start(process.env.PORT);
   });
 
